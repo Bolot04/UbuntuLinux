9e116d46cd0261bccd344bddb8816b3ca1995656
Bolot04 <bola2004bol@gmail.com>
2025-12-07 23:43:51 +0600

    update

--- main.py
+++ main.py
@@ -26,26 +26,67 @@ def save_to_db(name: str, timestamp: str):
 
 def main(page: ft.Page):
     page.title = "Что вроде чего то!!)"
+    page.theme_mode = ft.ThemeMode.LIGHT
+
+
+
+    greeting_text = ft.Text(value="Hello, World")
     create_name = ft.TextField(label="Введите имя!")
     result_text = ft.Text()
+
+    #----- Переключатель темы от чегото на чтото!!) -----
+    def toggle_theme(_):
+        if page.theme_mode == ft.ThemeMode.LIGHT:
+            page.theme_mode = ft.ThemeMode.DARK
+        else:
+            page.theme_mode = ft.ThemeMode.LIGHT
+
+        page.update()
+
+    theme_button = ft.IconButton(
+        icon=ft.Icons.BRIGHTNESS_7,
+        tooltip="Сменить тему",
+        on_click=toggle_theme
+    )
+
+
+
+
+
+
 
 
     def on_submit(_):
         name = create_name.value
+        # time_str = datetime.now().strftime("%Y:%m:%d - %H:%M:%S")
+        # save_to_db(name, time_str)
+        # result_text.value = f"Имя: {name}"
+
+        if not name:
+            greeting_text.value = "Введите корректное имя"
+            result_text.value = ""
+            page.update()
+            return
+
         time_str = datetime.now().strftime("%Y:%m:%d - %H:%M:%S")
         save_to_db(name, time_str)
-        result_text.value = f"Имя: {name}"
+
+
+        greeting_text.value = f"Hello, {name}"
+        result_text.value = f"Имя сохраненно: {name}"
         page.update()
 
     page.add(
+        greeting_text,
         create_name,
         ft.ElevatedButton("Отправить", on_click=on_submit),
-        result_text
+        result_text,
+        theme_button
     )
 
 create_db()
 
-ft.app(target=main)
+ft.app(target=main, )
 
 
 
