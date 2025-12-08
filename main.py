203bb1106cb440babb3ffd22a7dd8e0b0a716d27
Bolot04 <bola2004bol@gmail.com>
2025-12-08 18:40:50 +0600

    correction hw_3

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
 import flet as ft
 from datetime import datetime
-import sqlite3
-
-def create_db():
-    conn = sqlite3.connect("user_register_time.db")
-    cursor = conn.cursor()
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        timestamp TEXT                      
-                   )
-    """)
-    conn.commit()
-    conn.close()
-
-
-def save_to_db(name: str, timestamp: str):
-    conn = sqlite3.connect("user_register_time.db")
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (name, timestamp) VALUES (?, ?)", (name, timestamp))
-    conn.commit()
-    conn.close()
-
 
 def main(page: ft.Page):
     page.title = "Что вроде чего то!!)"
@@ -33,6 +10,7 @@ def main(page: ft.Page):
     greeting_text = ft.Text(value="Hello, World")
     create_name = ft.TextField(label="Введите имя!")
     result_text = ft.Text()
+    data_text = ft.Text(" ")
 
     #----- Переключатель темы от чегото на чтото!!) -----
     def toggle_theme(_):
@@ -50,26 +28,20 @@ def main(page: ft.Page):
     )
 
 
-
-
-
-
-
-
     def on_submit(_):
         name = create_name.value
+        time_str = datetime.now().strftime("%Y:%m:%d - %H:%M:%S")
         # time_str = datetime.now().strftime("%Y:%m:%d - %H:%M:%S")
         # save_to_db(name, time_str)
         # result_text.value = f"Имя: {name}"
+
+        data_text.value = f"Дата: {time_str}" 
 
         if not name:
             greeting_text.value = "Введите корректное имя"
             result_text.value = ""
             page.update()
             return
-
-        time_str = datetime.now().strftime("%Y:%m:%d - %H:%M:%S")
-        save_to_db(name, time_str)
 
 
         greeting_text.value = f"Hello, {name}"
@@ -81,10 +53,9 @@ def main(page: ft.Page):
         create_name,
         ft.ElevatedButton("Отправить", on_click=on_submit),
         result_text,
+        data_text,
         theme_button
     )
-
-create_db()
 
 ft.app(target=main, )
 
